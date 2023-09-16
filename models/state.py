c6b9893b5cc4f8458c51c43f92979b631e79704b
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-16 08:36:01 -0800

    updated state code

--- models/state.py
+++ models/state.py
@@ -5,4 +5,10 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = "Some State Name"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return "State {}: {}".format(self.id, self.name)
