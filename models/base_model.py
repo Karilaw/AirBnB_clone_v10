bacf46df48fc8015e984c7ec8118ccb2e04843fd
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-16 07:41:02 -0800

    update Base class to handle update_at correctly

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,28 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            updated_at = kwargs.pop('updated_at', None)
+            created_at = kwargs.pop('created_at', None)
+            if updated_at is None:
+                self.updated_at = self.created_at = datetime.now()
+            else:
+                self.updated_at = datetime.strptime(
+                    updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            if created_at is None:
+                self.created_at = datetime.now()
+            else:
+                self.created_at = datetime.strptime(
+                    created_at, '%Y-%m-%dT%H:%M:%S.%f')
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
