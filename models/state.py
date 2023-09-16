d4c0d686301629ad980a89ab0a915565939b54dd
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-16 08:48:13 -0800

    updated my state code

--- models/state.py
+++ models/state.py
@@ -9,6 +9,8 @@ class State(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if not kwargs.get('id'):
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
         return "State {}: {}".format(self.id, self.name)
