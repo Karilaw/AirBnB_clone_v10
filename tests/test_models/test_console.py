afa0adebaca8cc87936a094e4d1549e7f5b46a41
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-17 10:15:07 -0700

    added test_unit for do_create

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -2,39 +2,49 @@
 """ test case for console module"""
 
 import unittest
-from unittest.mock import patch
-import io
-import sys
-
-# Import your console.py file
+from io import StringIO
+# Assuming HBNBCommand is the class containing do_create
 from console import HBNBCommand
 
 
-class TestConsoleCreateWithParams(unittest.TestCase):
+class TestDoCreateCommand(unittest.TestCase):
     def setUp(self):
         """Set up a clean instance of the console before each test."""
-        self.console = HBNBCommand()
+        self.console = HBNBCommand(stdin=StringIO(), stdout=StringIO())
 
     def tearDown(self):
         """Clean up resources after each test."""
-        del self.console
+        self.console = None
 
-    def test_create_state_with_params(self):
-        """Test creating a State object with parameters."""
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.console.onecmd('create State name="California"')
-            output = mock_stdout.getvalue().strip()
-            self.assertIsNotNone(output)
-            self.assertNotEqual(output, '')
+    def test_create_valid_class(self):
+        """Test creating objects of valid classes."""
+        class_names = ["BaseModel", "User", "State",
+                       "City", "Amenity", "Place", "Review"]
+        for class_name in class_names:
+            args = f"{class_name} name='Test' number=42"
+            self.console.do_create(args)
+            output = self.console.stdout.getvalue().strip()
+            self.assertTrue(output.startswith("[" + class_name + "] ("))
+            self.assertTrue("Test" in output)
+            self.assertTrue("42" in output)
+            self.console.stdout.truncate(0)  # Reset stdout
 
-    def test_create_place_with_params(self):
-        """Test creating a Place object with parameters."""
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
-            self.console.onecmd(
-                'create Place city_id="0001" user_id="0001" name="My_little_house" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297')
-            output = mock_stdout.getvalue().strip()
-            self.assertIsNotNone(output)
-            self.assertNotEqual(output, '')
+    def test_create_invalid_class(self):
+        """Test creating objects of invalid classes."""
+        invalid_class_names = ["InvalidClass", "AnotherInvalidClass"]
+        for class_name in invalid_class_names:
+            args = f"{class_name} name='Test' number=42"
+            self.console.do_create(args)
+            output = self.console.stdout.getvalue().strip()
+            self.assertEqual(output, "** class doesn't exist **")
+            self.console.stdout.truncate(0)  # Reset stdout
+
+    def test_create_missing_class(self):
+        """Test creating objects with missing class name."""
+        args = ""
+        self.console.do_create(args)
+        output = self.console.stdout.getvalue().strip()
+        self.assertEqual(output, "** class name missing **")
 
 
 if __name__ == '__main__':
