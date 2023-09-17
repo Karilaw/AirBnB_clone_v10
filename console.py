d02d77fe5a1c7ca70c8166719ed1f77249f63642
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-17 06:34:27 -0700

    update the do_create function

--- console.py
+++ console.py
@@ -116,34 +116,33 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        args = args.split()
-        if not args:
-            print("** class name missing **")
-        class_name = args[0]
-        params = args[1:]
-        if class_name not in self.classes:
-            print("** class doesn't exist **")
-            return
+        try:
+            if not args:
+                raise SyntaxError()
+            my_list = args.split(" ")
 
-        attr_dict = {}
-        for param in params:
-            key, value = param.split('=')
-            if value.startswith('"') and value.endswith('"'):
-                attr_dict[key] = value[1:-
-                                       1].replace('\\"', '"').replace('_', ' ')
-            elif '.' in value:
-                try:
-                    attr_dict[key] = float(value)
-                except ValueError:
-                    print(f"Invalid float value for {key}: {value}")
+            params = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace('_', ' ')
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, ValueError):
+                        continue
+                params[key] = value
+            if params == {}:
+                obj = eval(my_list[0])()
             else:
-                try:
-                    attr_dict[key] = int(value)
-                except ValueError:
-                    print(f"Invalid int value for {key}: {value}")
-        new_instance = self.classes[class_name](**attr_dict)
-        new_instance.save()
-        print(new_instance)
+                obj = eval(my_list[0])(**params)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
     def help_create(self):
         """ Help information for the create method """
