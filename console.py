89d9277c327e8fb37472e7354c1c2581124d6112
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-17 04:39:48 -0700

    updated do_all in console

--- console.py
+++ console.py
@@ -220,17 +220,8 @@ class HBNBCommand(cmd.Cmd):
         """ Shows all objects, or all objects of a class"""
         print_list = []
 
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
-        else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+        for k, v in storage._FileStorage__objects.items():
+            print_list.append(str(v))
 
         print(print_list)
 
