53f42e80a9efd949af8a34e077f472ab7e1d3d03
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-18 02:24:09 -0700

    update password for my sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,11 +2,12 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create or Use the User and Set Password
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hnbn_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 --Grant Privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT Privilege on the performance_schema Database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
 FLUSH PRIVILEGES;
