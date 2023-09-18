20f1dce6f4632cd1b3ea8c4d50e49d06e9b90cc3
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-09-18 03:23:32 -0700

    updated my sql file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -9,7 +9,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create or Use the User and Set Password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Grant Privileges
+-- Grant Privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT Privilege on the performance_schema Database
